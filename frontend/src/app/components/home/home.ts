514676af191c12b018b560b2fe13d0abd64551bc
lortkipa <nikusha191208@gmail.com>
2026-05-19 16:25:51 +0400

    hero page design for desktop

--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Hero } from "./hero/hero";
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [],
+  imports: [Hero],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
