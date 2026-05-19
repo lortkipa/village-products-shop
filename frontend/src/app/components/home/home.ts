763a22dda65ce32d09e0ecfea01104168d485349
lortkipa <nikusha191208@gmail.com>
2026-05-19 17:14:30 +0400

    displaying products (design done)

--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from "./hero/hero";
+import { Products } from "./products/products";
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero, Products],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
