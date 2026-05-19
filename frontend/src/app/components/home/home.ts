4525b52d202af686eec9cf39dee82a622955c902
lortkipa <nikusha191208@gmail.com>
2026-05-19 17:35:36 +0400

    about section design

--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Hero } from "./hero/hero";
 import { Products } from "./products/products";
+import { About } from "./about/about";
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [Hero, Products],
+  imports: [Hero, Products, About],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
