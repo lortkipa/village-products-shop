a74a0bbe3964684c1fecd32a3072adca85b01bc0
lortkipa <nikusha191208@gmail.com>
2026-05-20 12:53:23 +0400

    why-us section done

--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Hero } from "./hero/hero";
 import { Products } from "./products/products";
 import { About } from "./about/about";
+import { WhyUs } from "./why-us/why-us";
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [Hero, Products, About],
+  imports: [Hero, Products, About, WhyUs],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
---
+++ frontend/src/app/components/home/why-us/why-us.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-why-us',
+  imports: [],
+  templateUrl: './why-us.html',
+  styleUrl: './why-us.scss',
+})
+export class WhyUs {}
