ddfe45a2f445dcb6de464c7a770cd7fb039cf78c
lortkipa <nikusha191208@gmail.com>
2026-05-20 13:24:01 +0400

    contanct frontend done

---
+++ frontend/src/app/components/home/contanct/contanct.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-contanct',
+  imports: [],
+  templateUrl: './contanct.html',
+  styleUrl: './contanct.scss',
+})
+export class Contanct {}
--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -3,11 +3,12 @@ import { Hero } from "./hero/hero";
 import { Products } from "./products/products";
 import { About } from "./about/about";
 import { WhyUs } from "./why-us/why-us";
+import { Contanct } from "./contanct/contanct";
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [Hero, Products, About, WhyUs],
+  imports: [Hero, Products, About, WhyUs, Contanct],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
